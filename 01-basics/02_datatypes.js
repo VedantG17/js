c1581e9acdd261d14bd1dfd0ca7171ffbf799f6c
Vedant Gusain <111310448+VedantG17@users.noreply.github.com>
2024-09-07 20:24:47 +0000

    datatypes

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+2) //we are usinug nodejs and not browser
+
+//avoid ; in code for readability
+//code readability should be high
+
+console.log(3+3) 
+console.log("Vedant")
+
+let name = "Vedant" //string
+let age =20     //number
+let isLoggedIn =false //boolean
+let state = null
+
+
+//PRIMITIVE DATATYPE
+//number => 2 pow 53
+//bigint
+//string => '' and " "
+//boolean => true/false
+//null => standalone value
+// undefined => 
+//symbol =>unique 
+
+
+//OBJECT
+
+console.log(typeof null) // type of null is null
+ 
+console.log(typeof undefined) //undefined
+
+
+
