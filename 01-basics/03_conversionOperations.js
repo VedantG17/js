d21164119191663e0ed4a75f78e19e80e3b7b162
Vedant Gusain <111310448+VedantG17@users.noreply.github.com>
2024-09-10 07:33:22 +0000

    CONVERSIONS file

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -30,4 +30,32 @@ console.log(typeof stringNumber);
 let value = 3 
 let negvalue= -value
 console.log(negvalue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = "Vedant"
+let str3 = str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(+true)
+// console.log(true+) //error
+console.log(+"")
+
+let num1,num2,num3 
+num1 = num2 = num3 = 2+2
+
+//prefix and postfix
+let gameCounter = 100
+++gameCounter
+gameCounter++
+console.log(gameCounter);
+
 
