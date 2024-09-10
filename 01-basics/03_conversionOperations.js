b55a59b53be73fcdbb853436046b385b044d9b01
Vedant Gusain <111310448+VedantG17@users.noreply.github.com>
2024-09-10 05:44:53 +0000

    dependencies changed fordependabot.yml

--- 01-basics/03_conversionOperations.js
+++ 01-basics/03_conversionOperations.js
@@ -25,5 +25,9 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//************************* Operations************************************
 
+let value = 3 
+let negvalue= -value
+console.log(negvalue)
 
