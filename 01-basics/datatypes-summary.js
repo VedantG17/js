271c45454050b549c9689db82fb886ba6f9f447a
Vedant Gusain <111310448+VedantG17@users.noreply.github.com>
2024-09-11 21:33:18 +0000

    file structure change

---
+++ 01-basics/datatypes-summary.js
@@ -0,0 +1,50 @@
+// datatypes -> primitive and non primitive(ref type)
+
+// PRIMITIVE 
+//7 types : String,Number,Boolean, null,undefined,Symbol,BigInt
+
+const score = 100 
+const scoreValue =100.3
+const isLoggedIn = false
+const outsideTemp = null;
+let userEmail; //undefined
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id)
+console.log(anotherId)
+console.log(id===anotherId)
+const bigNumber = 123456712345n
+
+//Refernce Type (Non Primitive)
+//Arrays, Objects , Functions
+
+const heros = ["superman","batman","spiderman"] //array 
+
+let myObj = {
+    name:"vedant",
+    age:"20",
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof bigNumber)
+console.log(typeof outsideTemp) //outsideTemp was null but it gives type as object
+console.log(typeof myFunction)
+
+// Return type of variables in JavaScript
+// 1) Primitive Datatypes
+//        Number => number
+//        String  => string
+//        Boolean  => boolean
+//        null  => object
+//        undefined  =>  undefined
+//        Symbol  =>  symbol
+//        BigInt  =>  bigint
+
+// 2) Non-primitive Datatypes
+//        Arrays  =>  object
+//        Function  =>  function
+//        Object  =>  object
+
