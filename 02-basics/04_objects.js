0c11035fef173e5756a4b20d3d1a9aaffce109d7
Vedant Gusain <111310448+VedantG17@users.noreply.github.com>
2025-06-17 09:48:37 +0000

    additional things in objects

--- 02-basics/04_objects.js
+++ 02-basics/04_objects.js
@@ -1,2 +1,51 @@
-const tinderUser = new Object()
+const tinderUser = {}
+tinderUser.id = "123abc"
+tinderUser.name = "Sam"
+tinderUser.isLoggedIn = false;
+
+// console.log(tinderUser)
+
+const regularUser ={
+    email:"@some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"vedant",
+            lastname:"gusain"
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname) //? if something mightnot be there
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+const obj4 = {5:"a",6:"d"}
+// const obj3 = {obj1,obj2}
+const obj3 = Object.assign({},obj1,obj2,obj4)
+console.log(obj3)
+
+const obj5 = {...obj1,...obj2}
+console.log(obj5)
+
+const users =[
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:2,
+        email:"i@gmail.com"
+    },
+    {
+        id:3,
+        email:"j@gmail.com"
+    }
+]
+
+console.log(users[0].email)
 console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+console.log(tinderUser.hasOwnProperty('gender'))
