e075e66734b7bf28cd794f0ae9b7ebf04a411e3a
Vedant Gusain <111310448+VedantG17@users.noreply.github.com>
2025-06-17 07:40:31 +0000

    Add object basics examples in JS

---
+++ 02-basics/03_object.js
@@ -0,0 +1,36 @@
+//singleton => one of its type
+//object literal -> non singleton -> multiple instances
+//constructor ->singleton
+
+const mySym = Symbol("key1");
+const JsUser = {
+    name:"vedant",
+    "full name":"Vedant Gusain",
+    [mySym] : "mykey1",
+    age:21,
+    location:"India",
+    email:"abc@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+JsUser.email = "qwerty@gmail.com";
+// Object.freeze(JsUser)
+JsUser.email = "changed@gmail.com";
+// console.log(JsUser)
+JsUser.greeting = function(){
+    console.log("Hello hi ");
+}
+console.log(JsUser.greeting())
+
+JsUser.greetingTwo = function(){
+    console.log(`hello Js User ${this.name}`)
+}
+
+console.log(JsUser.greetingTwo())
+
+
