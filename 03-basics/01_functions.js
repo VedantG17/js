763155ddd7d3902ee84f0f281a3d22dbdb56d559
Vedant Gusain <111310448+VedantG17@users.noreply.github.com>
2025-07-03 11:17:52 +0000

    rest operator object and array input in function

--- 03-basics/01_functions.js
+++ 03-basics/01_functions.js
@@ -42,4 +42,30 @@ console.log(loginUserMessage()) //undefined if no arg provided
 
 
 
+function calculateCartPrice(val1 , val2 , ...num1){   // ... is rest operator = pack in a bundle also spread operator as per usage
+    return num1
+}
+console.log(calculateCartPrice(200,300,400,900));
 
+const user ={
+    username: "vedant",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user)
+
+handleObject({
+    username:"Sam",
+    price:299
+})
+
+const myNewArray =[200,400,100,600]
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200,600,943,93]));
